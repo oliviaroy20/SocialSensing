6a221b1b30fad3440cfe1ae6f8c82b3279ac0a4f
oliviaroy20 <oliviaroy20@gmail.com>
2019-04-08 11:31:57 -0400

    cleaned all the senator tweets

--- Senate_Tweets/clean_senate_tweets.py
+++ Senate_Tweets/clean_senate_tweets.py
@@ -2,8 +2,10 @@
 
 if __name__ == '__main__':
 	# files = ['JerryMoran.csv', 'LindseyGraham.csv', 'LisaMurkowski.csv', 'RandPaul.csv', 'SenatorCollins.csv', 'SenBobCorker.csv', 'SenDeanHeller.csv', 'SenMikeLee.csv', 'TomCotton.csv']
-	files = ['SenDanSullivan.csv', 'SenatorBurr.csv', 'SenatorTester.csv']
-	for file in files:
+	# files = ['SenDanSullivan.csv', 'SenatorBurr.csv', 'SenatorTester.csv']
+	for filename in os.listdir("."):
+	    if ".csv" not in filename:
+	        continue
 		with open(file, 'r') as f:
 			file_write = open("cleaned_tweets/" + file[:-4]+"_cleaned.csv", 'w')
 			lines = f.readlines()
